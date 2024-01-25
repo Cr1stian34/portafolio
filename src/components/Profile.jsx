d88e03a5d0a30aa20002ef800f454014965fcd61
unknown <cristian.muyudumbay@gmail.com>
2024-01-25 12:44:35 -0500

    Boton cv descargar

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,5 +1,17 @@
+import axios from "axios"
+import fileDownload from "js-file-download"
+import { Link } from 'react-router-dom';
 
 const Profile = () => {
+
+    const handleClick = (url, filename) => {
+       axios.get(url, {
+        responseType: "blob"
+       })
+       .then((res)=>{fileDownload(res.data, filename)})
+       .catch((err)=> console.log(err))
+    }
+
     return (
         <section id="SectionHome" className="profile__container pt-[60px] px-5 relative">
             <div className="progress"></div>
@@ -12,11 +24,11 @@ const Profile = () => {
                 </div>
                 <div className="z-10 pfResponsive">
                     <h2 className="text-[30px]">Ingeniero de Software / <span className="text-[#7127BA]">Desarrollador Web Full Stack</span></h2>
-                    <hr className=" my-4"/>
+                    <hr className=" my-4" />
                     <p className="underline text-[17px]">Un diseñador que</p>
                     <h2 className="text-[20px]">Juzga un libro por su <span className="text-[#7127BA]">portada</span>...</h2>
                     <p className="text-[11px]">Porque si la portada no te impresiona qué más puede</p>
-                    <button className="btnCv transition-colors">Descargar CV</button>
+                    <button className="btnCv transition-colors"> <a href="/CvCristian_Muyudumbay.pdf" download={"Cv Cristian Muyudumbay"} >Descargar Cv</a> </button>
                 </div>
             </div>
 
